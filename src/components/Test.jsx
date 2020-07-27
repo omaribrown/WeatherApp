2c7819902037cbd64d524d9e2e170fff5bbe20b4
Omari Brown <omaris.brown@gmail.com>
2020-07-27 00:55:47 -0400

    setting up props

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -15,21 +15,25 @@ class Test extends React.Component {
 
         this.state = {
             info: [],
+            high: [],
+            low: [],
+            icon: [],
+            day: [],
         }
     }
     
-    componentDidMount() {
-        axios.get('https://api.openweathermap.org/data/2.5/forecast?lat=33.749&lon=-84.388&appid=61ad8827b626c5fad8ae7a0ba951db32')
-        .then(res => this.setState({ info: res.data }))
-        // .then(res => console.log(this.state.info))
-        .catch(err => console.error(err.message))
+    // componentDidMount() {
+    //     axios.get('https://api.openweathermap.org/data/2.5/forecast?lat=33.749&lon=-84.388&appid=61ad8827b626c5fad8ae7a0ba951db32')
+    //     .then(res => this.setState({ info: res.data }))
+    //     // .then(res => console.log(this.state.info))
+    //     .catch(err => console.error(err.message))
 
-    }
+    // }
     render() {
         return (
             <div>
                 <h1>test</h1>
-                
+
             </div>
         )
     }
